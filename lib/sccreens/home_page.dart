cefd7d591e50fe58bedfe1f128e7a2c92330104a
RGTechPro <73791635+RGTechPro@users.noreply.github.com>
2022-01-31 22:01:11 +0530

    added card

--- lib/sccreens/home_page.dart
+++ lib/sccreens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:housy_task/widgets/card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -104,6 +105,10 @@ class HomePage extends StatelessWidget {
                     fontSize: 17,
                     fontWeight: FontWeight.bold),
               ),
+              SizedBox(
+                height: 10,
+              ),
+              TaskCard()
             ],
           ),
         ),
---
+++ lib/widgets/card.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:housy_task/constants/size_config.dart';
+
+
+class TaskCard extends StatelessWidget {
+  const TaskCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Container(
+      height: SizeConfig.screenHeight! * 0.45,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                offset: Offset(0, .5),
+                spreadRadius: .1,
+                blurRadius: 10),
+          ],
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+          border: Border.all(color: Colors.grey.withOpacity(.01))),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.grey.withOpacity(.15))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Icon(
+                      Icons.person,
+                      color: Color(0xfff65875),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Icon(
+                  Icons.more_vert,
+                  color: Colors.grey.withOpacity(0.5),
+                ),
+              ),
+            ]),
+            SizedBox(
+              height: SizeConfig.screenHeight! * 0.22,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 3),
+              child: Text(
+                '9 Tasks',
+                style: TextStyle(
+                    fontFamily: 'Roboto', color: Colors.black54, fontSize: 17),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Text(
+                'Personal',
+                style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54,
+                    fontSize: 30),
+              ),
+            ),
+            SizedBox(
+              height: SizeConfig.screenHeight! * 0.02,
+            ),
+           
+          ],
+        ),
+      ),
+    );
+  }
+}
